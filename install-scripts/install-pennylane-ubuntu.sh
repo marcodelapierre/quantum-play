aebc1fa9064bb9edaa44911e55b41d3f8d04dac7
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-01 14:19:58 +0800

    adding ubuntu install scripts for PL and QK
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ install-scripts/install-pennylane-ubuntu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# essential for reproducibility of installation
+tool_name="pennylane"
+pl_ver="0.32.0"
+
+# install pre-requisites
+sudo apt-get update
+sudo apt-get install -y \
+  python3 \
+  python3-pip \
+  python3-setuptools
+pip3 install --upgrade pip
+
+# internal variables - do not edit
+python_ver="$( python3 -V | cut -d ' ' -f 2 | cut -d . -f 1,2 )"
+install_dir="$(pwd)/$tool_name"
+lib_dir="$install_dir/local/lib/python${python_ver}/dist-packages"
+bin_dir="$install_dir/local/bin"
+#
+export PYTHONPATH="$lib_dir:$PYTHONPATH" 
+export PATH="$bin_dir:$PATH"
+
+# install
+mkdir -p $install_dir
+pip install --prefix="$install_dir" \
+  pennylane=="$pl_ver" \
+  pennylane-lightning=="$pl_ver" \
+  pennylane-lightning-kokkos=="$pl_ver"
+
+# configure for startup
+echo "export PYTHONPATH=\"$lib_dir:\$PYTHONPATH\"" >> $(eval echo ~${USERID})/.bashrc
+echo "export PATH=\"$bin_dir:\$PATH\"" >> $(eval echo ~${USERID})/.bashrc
+
