1411a7521a0425804484507c74f95d8837c72b31
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-04 13:05:32 +0800

    device_test_scripts: adding pennylane scripts
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ device_test_scripts/test-rotation-pennylane-lightning.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import pennylane as qml
+from pennylane import numpy as np
+
+
+dev1 = qml.device("lightning.qubit", wires=1)
+
+
+# basic circuit
+
+@qml.qnode(dev1, interface="autograd")
+def circuit(params):
+    qml.RX(params[0], wires=0)
+    qml.RY(params[1], wires=0)
+    return qml.expval(qml.PauliZ(0))
+
+print(circuit([0.54, 0.12]))
+
+
+# circuit gradient
+dcircuit = qml.grad(circuit, argnum=0)
+
+print(dcircuit([0.54, 0.12]))
+
+
+# optimising the circuit
+
+def cost(x):
+    return circuit(x)
+
+init_params = np.array([0.011, 0.012], requires_grad=True)
+
+opt = qml.GradientDescentOptimizer(stepsize=0.4)
+
+steps = 100
+params = init_params
+
+for i in range(steps):
+    # update the circuit parameters
+    params = opt.step(cost, params)
+    if (i + 1) % 5 == 0:
+        print("Cost after step {:5d}: {: .7f}".format(i + 1, cost(params)))
+
+print("Optimized rotation angles: {}".format(params))
