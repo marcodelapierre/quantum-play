27ef6ef9a5965c0e0d3a6744e7209ae4b669a178
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-08 16:06:44 +0800

    added install script for pennylane + qiskit backend
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ install_scripts/install-pennylane+qiskit-ubuntu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# essential for reproducibility of installation
+tool_name="qiskit"
+pl_ver="0.32.0"
+qk_ver="0.44.1"
+aer_ver="0.12.2"
+
+# install pre-requisites
+sudo apt-get update
+sudo apt-get install -y \
+  python3 \
+  python3-pip \
+  python3-setuptools
+pip3 install --upgrade pip
+
+# internal variables - do not edit
+python_ver="$( python3 -V | cut -d ' ' -f 2 | cut -d . -f 1,2 )"
+install_dir="$(pwd)/$tool_name"
+lib_dir="$install_dir/local/lib/python${python_ver}/dist-packages"
+bin_dir="$install_dir/local/bin"
+#
+export PYTHONPATH="$lib_dir:$PYTHONPATH" 
+export PATH="$bin_dir:$PATH"
+
+# install
+mkdir -p $install_dir
+pip install --prefix="$install_dir" \
+  pennylane=="$pl_ver" \
+  pennylane-lightning=="$pl_ver" \
+  pennylane-qiskit=="$pl_ver" \
+  qiskit="$qk_ver" \
+  qiskit-aer="$aer_ver"
+
+# configure for startup
+echo "export PYTHONPATH=\"$lib_dir:\$PYTHONPATH\"" >> $(eval echo ~${USERID})/.bashrc
+echo "export PATH=\"$bin_dir:\$PATH\"" >> $(eval echo ~${USERID})/.bashrc
+
