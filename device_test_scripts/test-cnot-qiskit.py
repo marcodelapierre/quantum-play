712db3f2eceb61f284350cf48acc03f6a7de829a
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-08 17:10:16 +0800

    updated device test, qiskit
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- device_test_scripts/test-cnot-qiskit.py
+++ device_test_scripts/test-cnot-qiskit.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from qiskit import QuantumCircuit, Aer, BasicAer
+from qiskit import QuantumCircuit
+from qiskit_aer import AerSimulator
 from qiskit.compiler import transpile
-from qiskit.quantum_info import Statevector
+
+def create_circuit():
+    circ = QuantumCircuit(3)
+    circ.h(0)
+    circ.cx(0, 1)
+    circ.cx(0, 2)
+    return circ
 
 
 # build and visualise a circuit
-circ = QuantumCircuit(3)
-circ.h(0)
-circ.cx(0, 1)
-circ.cx(0, 2)
+circ = create_circuit()
+circ.save_statevector()
 
 circ.draw('text')
 
-
 # Run the quantum circuit on the Aer statevector simulator backend
-backend = Aer.get_backend('statevector_simulator')
+backend = AerSimulator(method='statevector')
+circ = transpile(circ, backend)
 
 job = backend.run(circ)
 result = job.result()
@@ -25,8 +30,13 @@ outputstate = result.get_statevector(circ, decimals=3)
 print(outputstate)
 
 
+# rebuild circuit for unitary method
+circ = create_circuit()
+circ.save_unitary()
+
 # Run the quantum circuit on the Aer unitary simulator backend
-backend = Aer.get_backend('unitary_simulator')
+backend = AerSimulator(method='unitary')
+circ = transpile(circ, backend)
 
 job = backend.run(circ)
 result = job.result()
@@ -37,9 +47,8 @@ print(outputstate)
 
 
 
-
-
 # create a quantum circuit with measurements
+circ = create_circuit()
 meas = QuantumCircuit(3, 3)
 meas.barrier(range(3))
 meas.measure(range(3), range(3))
@@ -49,22 +58,13 @@ qc = circ.compose(meas)
 
 qc.draw('text')
 
+# Run the quantum circuit on the automatic Aer backend
+backend = AerSimulator()
+circ = transpile(qc, backend)
 
-# Run the quantum circuit on the Aer qasm_simulator backend
-backend_sim = Aer.get_backend('qasm_simulator')
+job = backend.run(circ, shots=1024)
+result = job.result()
 
-job_sim = backend_sim.run(transpile(qc, backend_sim), shots=1024)
-result_sim = job_sim.result()
-
-counts = result_sim.get_counts(qc)
+counts = result.get_counts(qc)
 print(counts)
 
-
-# Run the quantum circuit on the BasicAer qasm_simulator backend
-backend_sim = BasicAer.get_backend('qasm_simulator')
-
-job_sim = backend_sim.run(transpile(qc, backend_sim), shots=1024)
-result_sim = job_sim.result()
-
-counts = result_sim.get_counts(qc)
-print(counts)
