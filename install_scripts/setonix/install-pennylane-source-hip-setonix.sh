dc4875b5b0345b9de29c20a41754a1b00b6a4e7e
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-13 14:38:12 +0800

    finally - fix for installing PL source HIP
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- install_scripts/setonix/install-pennylane-source-hip-setonix.sh
+++ install_scripts/setonix/install-pennylane-source-hip-setonix.sh
@@ -34,21 +34,43 @@ git checkout v$pl_ver
 sed -i 's;GIT_TAG *3\.7.*;GIT_TAG        4.1.00;g' CMakeLists.txt
 rm -fr build PennyLane_Lightning_Kokkos.egg-info
 
-CMAKE_ARGS="-DCMAKE_CXX_COMPILER=hipcc \
-  -DCMAKE_PREFIX_PATH=\"$install_dir\" \
-  -DPLKOKKOS_ENABLE_NATIVE=ON \
-  -DKokkos_ENABLE_HIP=ON \
-  -DKokkos_ARCH_VEGA90A=ON" \
-  pip install --prefix=$install_dir .
-
-#cmake -B build . \
-#  -DCMAKE_CXX_COMPILER=hipcc \
-#  -DCMAKE_PREFIX_PATH=\"$install_dir\" \
+#CMAKE_ARGS="-DCMAKE_CXX_COMPILER=hipcc \
+#  -DCMAKE_BUILD_TYPE=Release \
+#  -DCMAKE_PREFIX_PATH=$install_dir \
 #  -DPLKOKKOS_ENABLE_NATIVE=ON \
 #  -DKokkos_ENABLE_HIP=ON \
-#  -DKokkos_ARCH_VEGA90A=ON
-#cmake --build build
+#  -DKokkos_ARCH_VEGA90A=ON" \
+#  pip install --prefix=$install_dir .
 
+cmake -B build . \
+  -DCMAKE_CXX_COMPILER=hipcc \
+  -DCMAKE_CXX_FLAGS=--gcc-toolchain=$(dirname $(which g++))/../snos \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH=$install_dir \
+  -DPLKOKKOS_ENABLE_NATIVE=ON \
+  -DKokkos_ENABLE_HIP=ON \
+  -DKokkos_ARCH_VEGA90A=ON
+#  -DCMAKE_VERBOSE_MAKEFILE=ON \
+#  -DPLKOKKOS_ENABLE_WARNINGS=ON \
+
+# Edric's workaround for __noinline__ build issue
+# Also requires --gcc-toolchain above
+files=$(grep -rl "#include <memory>" build)
+for file in $files; do
+  echo "Patching $file"
+  sed -i 's/#include <memory>/#ifdef __noinline__\
+      #define GCC12_RESTORE_NOINLINE\
+      #undef __noinline__\
+    #endif\
+    #include <memory>\
+    #ifdef GCC12_RESTORE_NOINLINE\
+      #undef GCC12_RESTORE_NOINLINE\
+      #define __noinline__ _attribute((noinline))\
+    #endif/g' $file
+done
+
+cmake --build build
+pip install --prefix=$install_dir .
 cd -
 
 # configure for startup
