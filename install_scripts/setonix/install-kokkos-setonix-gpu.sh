e8963592b2a36a237f616f14084f0c9ba286adb9
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-13 17:15:33 +0800

    install pl seto hip - still seemingly not using gpu
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ install_scripts/setonix/install-kokkos-setonix-gpu.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+#SBATCH --job-name=install-kokkos-setonix-gpu
+#SBATCH --account=pawsey0001-gpu
+#SBATCH --partition=gpu-dev
+#SBATCH --exclusive
+#SBATCH --ntasks=1
+#SBATCH --threads-per-core=1
+#SBATCH --gpus-per-node=8
+#SBATCH --time=00:30:00
+#SBATCH --output=out-%x
+
+download_kokkos="1"
+kokkos_version="3.7.02"
+suffix="-setonix-gpu"
+
+module load rocm/5.2.3
+module load craype-accel-amd-gfx90a
+
+if [ "$download_kokkos" != "0" ] ; then
+ rm -rf kokkos-cpu
+ git clone https://github.com/kokkos/kokkos kokkos${suffix}-src
+fi
+
+cd kokkos${suffix}-src
+git checkout $kokkos_version
+
+mkdir build
+cd build
+
+cmake .. \
+  -DCMAKE_INSTALL_PREFIX=$(pwd)/../../kokkos$suffix \
+  -DCMAKE_CXX_COMPILER=hipcc \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DKokkos_ENABLE_HIP=On \
+  -DKokkos_ARCH_ZEN3=On \
+  -DKokkos_ARCH_VEGA90A=On \
+  -DKokkos_ENABLE_HIP_MULTIPLE_KERNEL_INSTANTIATIONS=On
+
+make -j $SLURM_CPUS_PER_TASK
+sg pawsey0001 -c 'make install'
+
+
+
--- install_scripts/setonix/install-pennylane-source-hip-setonix.sh
+++ install_scripts/setonix/install-pennylane-source-hip-setonix.sh
@@ -17,21 +17,16 @@ git checkout v$pl_ver
 
 #CMAKE_ARGS="-DCMAKE_CXX_COMPILER=hipcc \
 #  -DCMAKE_BUILD_TYPE=Release \
-#  -DCMAKE_PREFIX_PATH=$install_dir \
-#  -DPLKOKKOS_ENABLE_NATIVE=ON \
-#  -DKokkos_ENABLE_HIP=ON \
-#  -DKokkos_ARCH_VEGA90A=ON" \
+#  -DCMAKE_PREFIX_PATH=$(pwd)/../../kokkos-setonix-gpu \
+#  -DPLKOKKOS_ENABLE_NATIVE=ON" \
 #  pip install --prefix=$install_dir .
 
 rm -fr build PennyLane_Lightning_Kokkos.egg-info
 cmake -B build . \
   -DCMAKE_CXX_COMPILER=hipcc \
-  -DCMAKE_CXX_FLAGS=--gcc-toolchain=$(dirname $(which g++))/../snos \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_PREFIX_PATH=$install_dir \
-  -DPLKOKKOS_ENABLE_NATIVE=ON \
-  -DKokkos_ENABLE_HIP=ON \
-  -DKokkos_ARCH_VEGA90A=ON
+  -DCMAKE_PREFIX_PATH=$(pwd)/../../kokkos-setonix-gpu \
+  -DPLKOKKOS_ENABLE_NATIVE=ON
 #  -DCMAKE_VERBOSE_MAKEFILE=ON \
 #  -DPLKOKKOS_ENABLE_WARNINGS=ON \
 
@@ -56,6 +51,6 @@ pip install --prefix=$install_dir .
 cd -
 
 # configure for startup
-echo "export PYTHONPATH=\"$lib_dir:\$PYTHONPATH\"" >> $(eval echo ~${USERID})/.bashrc
-echo "export PATH=\"$bin_dir:\$PATH\"" >> $(eval echo ~${USERID})/.bashrc
+#echo "export PYTHONPATH=\"$lib_dir:\$PYTHONPATH\"" >> $(eval echo ~${USERID})/.bashrc
+#echo "export PATH=\"$bin_dir:\$PATH\"" >> $(eval echo ~${USERID})/.bashrc
 
