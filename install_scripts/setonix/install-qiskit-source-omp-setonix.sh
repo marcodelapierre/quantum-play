c48d65db3bc142e4cb6332231f74399e2063b3b0
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-14 16:02:41 +0800

    qiskit aer omp via thrust working
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ install_scripts/setonix/install-qiskit-source-omp-setonix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. use-qiskit-source-omp-setonix.sh
+
+# install
+git clone https://github.com/Qiskit/qiskit-aer $source_dir/qiskit-aer
+cd $source_dir/qiskit-aer
+git checkout $aer_ver
+pip install --prefix=$install_dir -r requirements-dev.txt
+pip install --prefix=$install_dir pybind11[global]
+
+python ./setup.py bdist_wheel -- \
+  -DCMAKE_CXX_COMPILER=CC \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DAER_THRUST_BACKEND=OMP \
+  -DAER_DISABLE_GDR=True \
+  --
+
+pip install --prefix=$install_dir -U dist/qiskit_aer*.whl
+cd -
