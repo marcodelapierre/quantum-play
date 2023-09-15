93a8d6518e8527928453e538282e4e8783668fee
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-15 20:33:58 +0800

    updated use-qristal sciript in qristal_initial/
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- qristal_initial/use-qristal.sh
+++ qristal_initial/use-qristal.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-module load py-pip/22.2.2-py3.10.8 python/3.10.8
-module load cmake/3.21.4
+module load python/3.10.10
+module load py-pip/23.1.2-py3.10.10
+module load cmake/3.24.3
 module load eigen/3.4.0
-module load openblas/0.3.15
+module load openblas/0.3.21
 
-export WORKING_DIR="/software/projects/pawsey0001/mdelapierre/quantum/qristal-june"
+export WORKING_DIR="$(pwd)"
 export SOURCE_DIR="${WORKING_DIR}/source"
 export INSTALL_DIR="${WORKING_DIR}/install"
 export PIP_DIR="${WORKING_DIR}/pip"
