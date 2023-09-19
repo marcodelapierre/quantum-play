784318f51f4bbb6f3c653c011fbd25e784c58896
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-19 11:08:20 +0800

    install prereqs for topaz and garrawarla
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ install_scripts/topaz/install-prereqs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+py_ver="3.10.10"
+pip_ver="23.1.2"
+st_ver="68.0.0"
+
+module load maali/1.8.4
+mkdir -p $MYGROUP/software/centos7.6/modulefiles
+module use $MYGROUP/software/centos7.6/modulefiles
+
+sed -i "s;\"setuptools/.*\";\"setuptools/$st_ver\";g" pip.cyg
+
+maali -t Python -v $py_ver
+export MAALI_DEFAULT_PYTHON=python/$py_ver
+
+maali -t setuptools -v $st_ver
+maali -t pip -v $pip_ver
