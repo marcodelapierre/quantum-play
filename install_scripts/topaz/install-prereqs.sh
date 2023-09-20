f911442ca95355d7fafc780c27f4555192718164
Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-20 17:46:29 +0800

    topaz install, updated prereqs scripts
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

--- install_scripts/topaz/install-prereqs.sh
+++ install_scripts/topaz/install-prereqs.sh
@@ -12,6 +12,13 @@ module use $MYGROUP/software/centos7.6/modulefiles
 
 sed -i "s;\"setuptools/.*\";\"setuptools/$st_ver\";g" pip.cyg
 
+maali -t openssl -v 1.1.1k
+maali -t libffi -v 3.0.13 -d
+mkdir -p $MYGROUP/software/centos7.6/apps/gcc/4.8.5/libffi/3.0.13/include
+cp -p \
+  $MYGROUP/software/centos7.6/build/libffi-3.0.13/x86_64-unknown-linux-gnu/include/ffi*.h \
+  $MYGROUP/software/centos7.6/apps/gcc/4.8.5/libffi/3.0.13/include/
+
 maali -t Python -v $py_ver
 export MAALI_DEFAULT_PYTHON=python/$py_ver
 
