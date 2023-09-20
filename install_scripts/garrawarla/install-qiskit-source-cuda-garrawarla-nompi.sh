3e790b52bb9ee5decf8903a74bf10f8a03370348
Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-21 00:09:17 +0800

    garrawarla: starting qiskit setup
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ install_scripts/garrawarla/install-qiskit-source-cuda-garrawarla-nompi.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+#SBATCH --job-name=install-qiskit-source-cuda-topaz
+#SBATCH --account=pawsey0001
+#SBATCH --partition=gpuq-dev
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --time=00:30:00
+#SBATCH --output=out-%x
+
+. use-qiskit-source-cuda-garrawarla-nompi.sh
+
+# install
+git clone https://github.com/Qiskit/qiskit-aer $source_dir/qiskit-aer
+cd $source_dir/qiskit-aer
+git checkout $aer_ver
+pip install --prefix=$install_dir -r requirements-dev.txt
+pip install --prefix=$install_dir pybind11[global]
+
+pip install --prefix=$install_dir nvidia-cuda-runtime-cu11 nvidia-cublas-cu11 nvidia-cusolver-cu11 nvidia-cusparse-cu11 cuquantum-cu11
+
+python ./setup.py bdist_wheel -- \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DAER_MPI=False \
+  -DAER_THRUST_BACKEND=CUDA \
+  -DAER_CUDA_ARCH=7.0 \
+  -DAER_DISABLE_GDR=False \
+  -AER_PYTHON_CUDA_ROOT=$install_dir \
+  --
+
+pip install --prefix=$install_dir dist/qiskit_aer*.whl
+cd -
