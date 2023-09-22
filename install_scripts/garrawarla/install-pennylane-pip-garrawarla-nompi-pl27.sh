e3abf70375f2346594c870097b1251efddb8537a
Marco De La Pierre <marco.delapierre@gmail.com>
2023-09-22 13:31:42 +0800

    garrawarla: pl-light-gpu 0.27.0 working
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ install_scripts/garrawarla/install-pennylane-pip-garrawarla-nompi-pl27.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+. $script_dir/use-pennylane-pip-garrawarla-nompi-pl27.sh
+
+# install
+# this will get kokkos with Nvidia GPU backend
+mkdir -p $install_dir
+pip install --prefix="$install_dir" nvidia-cuda-runtime-cu11 nvidia-cublas-cu11 nvidia-cusolver-cu11 nvidia-cusparse-cu11 cuquantum-cu11
+pip install --prefix="$install_dir" \
+  pennylane=="$pl_ver" \
+  pennylane-lightning=="$pl_ver" \
+  pennylane-lightning-gpu=="$pl_ver"
